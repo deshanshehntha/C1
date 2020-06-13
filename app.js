ed5c0f3080ffdd1ef4e84d402c90c0517e2bdba0
deshanw <deshanshehantha@gmail.com>
2020-06-13 12:03:54 +0530

    c1 commit

--- app.js
+++ app.js
@@ -39,6 +39,10 @@
         /** create main connection variable**/
         var mainConnectionURL;
 
+        var MAIN_SERVER_CONNECTION =  "http://localhost:4003/";
+
+
+
 
         const ngrok = require('ngrok');
         (async function() {
@@ -49,12 +53,13 @@
             global.io = require('socket.io')(server);
             /** create socket io server */
 
+            var globalClientSocket = createNewPeerConnection(MAIN_SERVER_CONNECTION);
 
             global.io.on('connection', function (socket) {
                 console.log("Connected Socket | " + socket.id);
 
                 socket.on('own_client', function () {
-                    console.log("Connected this nodes own client |")
+                    console.log("Connected this nodes own client |");
                 });
 
                 socket.on('client_connection_request', function (data) {
@@ -63,7 +68,6 @@
                     AllConnectionsTestStore.add(socket.id, data.customId, data.ip, Date.now())
                     const address = leadershipSelectionAlgorithm(socket.id);
                     socket.emit('redirect_url', address);
-
                 });
 
                 socket.on('disconnect', function () {
@@ -81,66 +85,81 @@
 
                 });
 
-                socket.on('getting_connected_node_details_from_Children', function (data) {
-                    console.log("Node connection details | url : " + data.url + " Connection Data : " + data.childNodes);
-                    socket1.emit('getting_connected_node_details', data);
+                socket.on('connected_to_directed_node', function (data) {
+                    socket.emit("connected_to_directed_node")
                 });
 
+                // socket.on('getting_connected_node_details_from_Children', function (data) {
+                //     console.log("Node connection details | url : " + data.url + " Connection Data : " + data.childNodes);
+                //     globalClientSocket.emit('getting_connected_node_details', data);
+                // });
+
+
+                socket.on('getting_connected_node_details', function (data) {
+                    console.log("Node connection details | url : " + data.url + " Connection Data : " + data.childNodes);
+                    globalClientSocket.emit('getting_connected_node_details', data);
+                });
+
+                socket.on('connected_to_directed_node', function (data) {
+                    globalClientSocket.emit('connected_to_directed_node');
+                });
             });
 
 
-            /** create socket io client*/
-
-            let client = require("socket.io-client");
-            var socket1 = client.connect("http://127.0.0.1:4003");
-
-
-            socket1.on('connect', function () {
-                socket1.emit('client_connection_request', {customId: SERVER_ID, ip: connectionURL});
-                console.log("Conneected to the main node |");
-                // console.log("Owm connection parameters : " + SERVER_ID + ip.address() + server.address().port);
-            });
-
-            socket1.on("redirect_url", (data) => {
-                console.log("Getting the redirection parameters from main node...... |");
-                if(data !== 1) {
-                    mainConnectionURL = data;
-                    console.log("Redirect params from the server |" + data);
-                    console.log("Main connection URL setted |" + mainConnectionURL);
-                    console.log("Disconnecting with the main server |");
-                    socket1.disconnect()
-                    createNewPeerConnection(data)
-
-                } else {
-                    console.log("Stay connected to main server |");
-                }
-
-            });
-
-            socket1.on('requesting_connection_details', function () {
-                console.log("Getting connection nodes.....");
-                io.emit('requesting_connection_details');
-                console.log("Getting connected nodes from the main parent node|");
-                socket1.emit('getting_connected_node_details', { "url" : connectionURL, "childNodes" : ClientDataStore.getAll()});
-            });
+            // /** create socket io client*/
+            //
+            // let client = require("socket.io-client");
+            // var socket1 = client.connect("https://enigmatic-spire-15495.herokuapp.com/");
+            //
+            //
+            // socket1.on('connect', function () {
+            //     socket1.emit('client_connection_request', {customId: SERVER_ID, ip: connectionURL});
+            //     console.log("Conneected to the main node |");
+            //     // console.log("Owm connection parameters : " + SERVER_ID + ip.address() + server.address().port);
+            // });
+            //
+            // socket1.on("redirect_url", (data) => {
+            //     console.log("Getting the redirection parameters from main node...... |");
+            //     if(data !== 1) {
+            //         mainConnectionURL = data;
+            //         console.log("Redirect params from the server |" + data);
+            //         console.log("Main connection URL setted |" + mainConnectionURL);
+            //         console.log("Disconnecting with the main server |");
+            //         socket1.disconnect()
+            //         createNewPeerConnection(data)
+            //
+            //     } else {
+            //         console.log("Stay connected to main server |");
+            //     }
+            //
+            // });
+            //
+            // socket1.on('requesting_connection_details', function () {
+            //     console.log("Getting connection nodes.....");
+            //     io.emit('requesting_connection_details');
+            //     console.log("Getting connected nodes from the main parent node|");
+            //     socket1.emit('getting_connected_node_details', { "url" : connectionURL, "childNodes" : ClientDataStore.getAll()});
+            // });
 
 
             function createNewPeerConnection(data) {
-                console.log("Create new peer connecton with redirectd node | " + data);
 
-                var socket1 = require('socket.io-client')(""+data+"", {
+                var socket2 =  require('socket.io-client')(""+data+"", {
                         forceNew: true
                     });
 
-                socket1.on('connect', function (data) {
-                    socket1.emit('client_connection_request', {customId: SERVER_ID, ip: connectionURL});
-                    console.log("Connect to redirected node |");
+                globalClientSocket = socket2;
+
+
+                socket2.on('connect', function (data) {
+                    socket2.emit('client_connection_request', {customId: SERVER_ID, ip: connectionURL});
+                    console.log("Connect to parent server node |");
                 });
 
-                socket1.on('disconnect', function () {
-                    console.log("Disconnected from |" + socket1.io.uri);
-                    console.log(mainConnectionURL + "...Comparing..." + socket1.io.uri);
-                    var testUrl = socket1.io.uri;
+                socket2.on('disconnect', function () {
+                    console.log("Disconnected from |" + socket2.io.uri);
+                    // console.log(mainConnectionURL + "...Comparing..." + socket2.io.uri);
+                    var testUrl = socket2.io.uri;
                     var compareValue = mainConnectionURL.localeCompare(testUrl);
                     console.log("compared result" + compareValue)
                     if(mainConnectionURL === testUrl) {
@@ -149,38 +168,41 @@
                     }
                 });
 
-                socket1.on("redirect_url", (data) => {
+                socket2.on("redirect_url", (data) => {
                     if(data !== 1) {
                         mainConnectionURL = data;
                         console.log("Main connection URL setted |" + mainConnectionURL );
                         console.log("Redirect params from the redirected nodes |" + data);
                         console.log("Disconnecting with the main server");
                         console.log("redirect url" + data);
-                        socket1.disconnect()
-                        createNewPeerConnection(data)
+                        socket2.disconnect()
+                        globalClientSocket = createNewPeerConnection(data)
 
+                    } else {
+                        socket2.emit("connected_to_directed_node")
                     }
                 });
 
 
-                socket1.emit('from_child', "test");
+                socket2.emit('from_child', "test");
 
-                socket1.on('requesting_connection_details', function () {
+                socket2.on('requesting_connection_details', function () {
                     console.log("Getting connection nodes.....");
                     io.emit('requesting_connection_details');
-                    console.log("Getting connected nodes from child|");
-                    socket1.emit('getting_connected_node_details_from_Children', { "url" : connectionURL, "childNodes" : ClientDataStore.getAll()});
+                    console.log("Getting connected nodes from a peer node|");
+                    // socket2.emit('getting_connected_node_details_from_Children', { "url" : connectionURL, "childNodes" : ClientDataStore.getAll()});
+                    socket2.emit('getting_connected_node_details', { "url" : connectionURL, "childNodes" : ClientDataStore.getAll()});
 
                 });
 
-
+                return socket2;
             }
 
             function onDisconnectConnectToMain() {
 
                 console.log("Connect back to main......")
 
-                var socket1 = require('socket.io-client')("http://127.0.0.1:4003", {
+                var socket1 = require('socket.io-client')("https://enigmatic-spire-15495.herokuapp.com/", {
                     forceNew: true
                 });
 
